112a9054559b13cc1e8517db337673d85b17ffbb
SolidDogor <olivosralejandromatias@gmail.com>
2025-06-28 21:22:44 -0500

    Ejercicio 28: Agregando buscador de número

--- Repaso-CPP-2025/ejercicio28.cpp
+++ Repaso-CPP-2025/ejercicio28.cpp
@@ -3,9 +3,10 @@
 int* crearArreglo(int n);
 void llenarArreglo(int* arr, int n);
 int sumarArreglo(int* arreglo, int cantidad);
+bool encontrarNumero(int* arreglo, int cantidad, int numero);
 
 int main(){
-    int cantidad = 0;
+    int cantidad = 0, numero = 0;
     std::cout << "Ingrese la cantidad de numeros dentro del arreglo: ";
     std::cin >> cantidad;
     int* arr = crearArreglo(cantidad);
@@ -15,6 +16,14 @@ int main(){
 
     std::cout << "La cantidad de la suma dentro del arreglo es: " << suma << std::endl;
     
+    std::cout << "Que numero desea buscar?" << std::endl;
+    std::cin >> numero;
+    if(encontrarNumero(arr,cantidad,numero)) {
+        std::cout << "El numero si se encuentra dentro del arreglo!" << std::endl;
+    } else {
+        std::cout << "Numero no encontrado dentro de la funcion..." << std::endl;
+    }
+
     delete[] arr;
     return 0;
 }
@@ -37,4 +46,13 @@ int sumarArreglo(int* arreglo, int cantidad){
     }
 
     return suma;
+}
+
+bool encontrarNumero(int* arreglo, int cantidad, int numero){
+    for (int i = 0; i < cantidad; i++) {
+        if (arreglo[i] == numero) {
+            return true;
+        }
+    }
+    return false;
 }
